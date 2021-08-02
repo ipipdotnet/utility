0818d13e208a7aff78a996b64c89ce2c15de27d6
frk <frk@ipip.net>
2021-08-02 17:43:26 +0800

    update

--- gpsutil/gps.go
+++ gpsutil/gps.go
@@ -1,8 +1,14 @@
 package gpsutil
 
-// https://github.com/muesli/kmeans
-
 type GPS struct {
 	Longitude float64
 	Latitude float64
+}
+
+func (g GPS) Distance(g2 GPS) float64 {
+	return Distance(g, g2)
+}
+
+func Center(gs []GPS) GPS {
+	return GPS{}
 }
